8208a68865b89b457d6ee4093ec96868a9830727
Ryu732 <sakanaction001@gmail.com>
2024-09-12 00:05:26 +0900

    こっみとテスト

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="#keypad-enter">test</div>
+	<div id="#keypad-enter">test</div>
+	<v-app>
+		<v-main>
+			<v-container>
+				<!-- 電話番号表示 -->
+				<v-row>
+					<v-col v-for="i in 11" :key="i" cols="1">
+						<div class=".o-input--num-display">
+							{{ phoneNumber[i - 1] }}
+						</div>
+					</v-col>
+				</v-row>
+				<!-- 入力ボタン -->
+				<v-row>
+					<v-col v-for="i in 9" :key="i" cols="4">
+						<v-btn color="primary" @click="addPhoneNumber(i)" :class="`#keypad-${i}`"> {{ i }}</v-btn>
+					</v-col>
+					<v-col cols="4">
+						<v-btn color="primary" @click="deletePhoneNumber" :class="'#keypad-delete'">D</v-btn>
+					</v-col>
+					<v-col cols="4">
+						<v-btn color="primary" @click="addPhoneNumber(0)">0</v-btn>
+					</v-col>
+					<v-col cols="4">
+						<v-btn color="primary" @click="sendPhoneNumber" :disabled="enterDisabled"
+							:class="'#keypad-enter'">E</v-btn>
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { reactive, ref, watch } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+// 入力された電話番号
+const phoneNumber = reactive([]);
+
+// enterが押せるかどうかのフラグ
+const enterDisabled = ref(true);
+
+// 電話番号を追加
+function addPhoneNumber(num) {
+	if (phoneNumber.length < 11) {
+		phoneNumber.push(num);
+	}
+}
+
+// 電話番号を削除
+function deletePhoneNumber() {
+	phoneNumber.pop();
+}
+
+// 電話番号が送信できるかの判定
+watch(phoneNumber, () => {
+	// 電話番号が10桁で0から始まる時は送信可能
+	if (phoneNumber.length === 10 && phoneNumber[0] === 0) {
+		enterDisabled.value = false;
+	} else if (phoneNumber.length === 11 && phoneNumber[0] === 0 && phoneNumber[2] === 0) {
+		// 電話番号が11桁で0から始まる時は送信可能
+		enterDisabled.value = false;
+	} else {
+		enterDisabled.value = true;
+	}
+});
+
+function sendPhoneNumber() {
+	// 送信処理
+	alert(phoneNumber);
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.o-input--num-display {
+	font-size: 20px;
 }
 </style>
